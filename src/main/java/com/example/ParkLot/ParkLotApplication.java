7e3314eaee3efbb4614b21f838c7416a16eca4c6
Andre Mangara <andremangaras@gmail.com>
2020-04-25 20:12:50 +0700

    First Commit

---
+++ src/main/java/com/example/ParkLot/ParkLotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParkLot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkLotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkLotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ParkLot/ParkLotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParkLot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
